2e603a0b9524c50e8e3c1a837ccb8dad1dcb0162
remuslum <57741494+remuslum@users.noreply.github.com>
2024-10-30 14:50:59 +0800

    Print Gameboard

--- task02/run.sh
+++ task02/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 javac -d ./classes -sourcepath ./src src/vttp/batch5/sdf/task02/Main.java && \
-	java -cp classes vttp.batch5.sdf.task02.Main files/board3.txt
+	java -cp classes vttp.batch5.sdf.task02.Main TTT/board3.txt
--- task02/src/vttp/batch5/sdf/task02/moves/Move.java
+++ task02/src/vttp/batch5/sdf/task02/moves/Move.java
@@ -3,85 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Move {
-    private char[][] board;
+    private final char[][] board;
 
     public Move(char[][] board){
         this.board = board;
-    }
-
-    public List<List<Integer>> getEmptyPositions(){
-        List<List<Integer>> allEmptyPositions = new ArrayList<>();
-        for(int x = 0; x < board.length; x++){
-            for(int y = 0; y < board[0].length; y++){
-                List<Integer> row = new ArrayList<>();
-                if(board[x][y] == '.'){
-                    row.add(x);
-                    row.add(y);
-                    allEmptyPositions.add(row);
-                } 
-            }
-        }
-        return allEmptyPositions;
-    }
-
-    public char[][] createBoardCopy(){
-        char[][] boardCopy = new char[board.length][board[0].length];
-        for(int i = 0; i < board.length; i++){
-            System.arraycopy(board[i], 0, boardCopy[i], 0, board[0].length);
-        }
-        return boardCopy;
-    }
-
-    public char[][] place(char player, List<Integer> position){
-        char[][] copyOfBoard = createBoardCopy();
-        copyOfBoard[position.get(0)][position.get(1)] = player;
-        return copyOfBoard;
-    }
-
-    public int evaluateRowWin(char[][] board){
-        //Evaluate row
-        for (char[] board1 : board) {
-            if ((board1[0] == board1[1]) && (board1[1] == board1[2])) {
-                return switch (board1[0]) {
-                    case 'X' -> 1;
-                    case 'O' -> -1;
-                    default -> 0;
-                };
-            }
-        }
-        return 0;
-    }
-
-    public int evaluateColumnWin(char[][] board){
-        //Evaluate column
-        for(int i = 0; i < board[0].length; i++){
-            if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
-                return switch (board[0][i]) {
-                    case 'X' -> 1;
-                    case 'O' -> -1;
-                    default -> 0;
-                };
-            }
-        }
-        return 0;   
-    }
-
-    public int evaluateDiagonalWin(char[][] board){
-        //Evaluate Diagonals
-        if(((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))){
-            return switch (board[0][0]) {
-                case 'X' -> 1;
-                case 'O' -> -1;
-                default -> 0;
-            };
-        } else if (((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))){
-            return switch (board[0][2]) {
-                case 'X' -> 1;
-                case 'O' -> -1;
-                default -> 0;
-            };
-        }
-        return 0;
     }
 
     public String generateUtility(){
@@ -115,5 +40,82 @@ public class Move {
         }
         return sb.toString();
     }
+
+    private List<List<Integer>> getEmptyPositions(){
+        List<List<Integer>> allEmptyPositions = new ArrayList<>();
+        for(int x = 0; x < board.length; x++){
+            for(int y = 0; y < board[0].length; y++){
+                List<Integer> row = new ArrayList<>();
+                if(board[x][y] == '.'){
+                    row.add(x);
+                    row.add(y);
+                    allEmptyPositions.add(row);
+                } 
+            }
+        }
+        return allEmptyPositions;
+    }
+
+    private char[][] createBoardCopy(){
+        char[][] boardCopy = new char[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++){
+            System.arraycopy(board[i], 0, boardCopy[i], 0, board[0].length);
+        }
+        return boardCopy;
+    }
+
+    private char[][] place(char player, List<Integer> position){
+        char[][] copyOfBoard = createBoardCopy();
+        copyOfBoard[position.get(0)][position.get(1)] = player;
+        return copyOfBoard;
+    }
+
+    private int evaluateRowWin(char[][] board){
+        //Evaluate row
+        for (char[] board1 : board) {
+            if ((board1[0] == board1[1]) && (board1[1] == board1[2])) {
+                return switch (board1[0]) {
+                    case 'X' -> 1;
+                    case 'O' -> -1;
+                    default -> 0;
+                };
+            }
+        }
+        return 0;
+    }
+
+    private int evaluateColumnWin(char[][] board){
+        //Evaluate column
+        for(int i = 0; i < board[0].length; i++){
+            if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
+                return switch (board[0][i]) {
+                    case 'X' -> 1;
+                    case 'O' -> -1;
+                    default -> 0;
+                };
+            }
+        }
+        return 0;   
+    }
+
+    private int evaluateDiagonalWin(char[][] board){
+        //Evaluate Diagonals
+        if(((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))){
+            return switch (board[0][0]) {
+                case 'X' -> 1;
+                case 'O' -> -1;
+                default -> 0;
+            };
+        } else if (((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))){
+            return switch (board[0][2]) {
+                case 'X' -> 1;
+                case 'O' -> -1;
+                default -> 0;
+            };
+        }
+        return 0;
+    }
+
+    
 
 }

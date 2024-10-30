56e3c1137d239a72183c035619ebea6375c65de9
remuslum <57741494+remuslum@users.noreply.github.com>
2024-10-30 14:20:01 +0800

    Handled Task2 errors

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -1,21 +1,22 @@
 package vttp.batch5.sdf.task02;
 
-import java.util.List;
 import vttp.batch5.sdf.task02.board.Board;
 import vttp.batch5.sdf.task02.moves.Move;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-
-		Board board = new Board();
-		char[][] gameBoard = board.generateBoard(args[0]);
-		Move move = new Move(gameBoard);
-		List<List<Integer>> emptyPositions = move.getEmptyPositions();
-		System.out.println(emptyPositions);
-
-		System.out.println(move.generateUtility());
-
-
+	public static void main(String[] args) {
+		try {
+			Board board = new Board();
+			char[][] gameBoard = board.generateBoard(args[0]);
+			if (gameBoard.length != 0){
+				System.out.println(board.printBoard(gameBoard));
+				Move move = new Move(gameBoard);
+				System.out.println(move.generateUtility());
+			}
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid TTT configuration file");
+		}
 	}
 }
--- task02/src/vttp/batch5/sdf/task02/board/Board.java
+++ task02/src/vttp/batch5/sdf/task02/board/Board.java
@@ -14,28 +14,48 @@ public class Board {
 
     }
 
-    public char[][] generateBoard(String textFile) throws IOException{
-        File file = new File(PATH + File.separator + textFile);
-        FileReader fileReader = new FileReader(file);
-        BufferedReader br = new BufferedReader(fileReader);
-        StringBuilder sb = new StringBuilder();
+    public char[][] generateBoard(String textFile){
+        try {
+            File file = new File(PATH + File.separator + textFile);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            StringBuilder sb = new StringBuilder();
 
-        String line = "";
-        int count = 0;
-        while((line = br.readLine()) != null){
-            sb.append(line);
-            if(count < 2){
-                sb.append(",");
+            String line = "";
+            int count = 0;
+            while((line = br.readLine()) != null){
+                if(!(line.contains(".") || line.contains("X") || line.contains("O"))){
+                    System.out.println("File does not contain TTT configuration");
+                    break;
+                }
+                sb.append(line);
+                if(count < 2){
+                    sb.append(",");
+                }
+                count++;
             }
-            count++;
+            String[] inputs = sb.toString().split(",");
+            for(int i = 0; i < inputs.length; i++){
+                char[] temp = inputs[i].toCharArray();
+                System.arraycopy(temp, 0, board[i], 0, temp.length);
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("File does not exist");
         }
-        String[] inputs = sb.toString().split(",");
-        for(int i = 0; i < inputs.length; i++){
-            char[] temp = inputs[i].toCharArray();
-            System.arraycopy(temp, 0, board[i], 0, temp.length);
-        }
-        br.close();
         return board;
     }
+
+    public String printBoard(char[][] gameBoard){
+        StringBuilder sb = new StringBuilder();
+        for(char[] row:gameBoard){
+            for(char space:row){
+                sb.append(space);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
 
 }
